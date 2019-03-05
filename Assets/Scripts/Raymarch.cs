15015651e3e078d5f7f665ad99bbc3d68ddb953f
LeithBA <leith.benabdessalem@ue-germany.de>
2019-03-05 09:28:03 +0100

    initial commit

---
+++ Assets/Scripts/Raymarch.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class Raymarch : SceneViewFilter
+{
+    [SerializeField] private Shader _shader;
+
+    public Material _raymarchMaterial
+    {
+        get
+        {
+            if (!_raymarchMat && _shader)
+            {
+                _raymarchMat = new Material(_shader);
+                _raymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return _raymarchMat;
+        }
+    }
+    private Material _raymarchMat;
+
+    public Camera _camera
+    {
+        get
+        {
+            if (!_cam)
+            {
+                _cam = GetComponent<Camera>();
+            }
+            return _cam;
+        }
+    }
+    private Camera _cam;
+
+	public Transform _directionLight;
+
+	public float _maxDistance;
+    public Color _mainColor;
+	public Vector4 _sphere1, _box1;
+    public Vector3 _modInterval;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!_raymarchMaterial)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+		_raymarchMaterial.SetVector("_LightDir", _directionLight ? _directionLight.forward : Vector3.down);
+        _raymarchMaterial.SetMatrix("_CamFrustum", CamFrustum(_camera));
+        _raymarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
+		_raymarchMaterial.SetFloat("_maxDistance", _maxDistance);
+		_raymarchMaterial.SetVector("_sphere1", _sphere1);
+        _raymarchMaterial.SetVector("_box1", _box1);
+        _raymarchMaterial.SetVector("_modInterval", _modInterval);
+        _raymarchMaterial.SetColor("_mainColor", _mainColor);
+
+        RenderTexture.active = destination;
+		_raymarchMaterial.SetTexture("_MainTex", source);
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        _raymarchMaterial.SetPass(0);
+        GL.Begin(GL.QUADS);
+
+        //BL
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 3.0f);
+
+        //BR
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 2.0f);
+
+        //TR
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
+
+        //TL
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+		GL.End();
+		GL.PopMatrix();
+    }
+
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((cam.fieldOfView * 0.5f) * Mathf.Deg2Rad);
+
+        Vector3 goUp = Vector3.up * fov;
+        Vector3 goRight = Vector3.right * fov * cam.aspect;
+
+        Vector3 TL = (-Vector3.forward - goRight + goUp);
+        Vector3 TR = (-Vector3.forward + goRight + goUp);
+        Vector3 BR = (-Vector3.forward + goRight - goUp);
+        Vector3 BL = (-Vector3.forward - goRight - goUp);
+
+        frustum.SetRow(0, TL);
+        frustum.SetRow(1, TR);
+        frustum.SetRow(2, BR);
+        frustum.SetRow(3, BL);
+
+        return frustum;
+    }
+}
---
+++ Assets/Shaders/Raymarch.shader
@@ -0,0 +1,126 @@
+﻿Shader "Custom/Raymarch"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 3.0
+			
+			#include "UnityCG.cginc"
+			#include "DistanceFunctions.cginc"
+
+			sampler2D _MainTex;
+			uniform sampler2D _CameraDepthTexture;
+			uniform float4x4 _CamFrustum, _CamToWorld;
+			uniform float _maxDistance;
+			uniform float4 _sphere1, _box1;
+			uniform float3 _modInterval;
+			uniform float3 _LightDir;
+			uniform fixed4 _mainColor;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				float3 ray : TEXCOORD1;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				half index = v.vertex.z;
+				v.vertex.z = 0;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+
+				o.ray = _CamFrustum[(int)index].xyz;
+
+				o.ray /= abs(o.ray.z);
+
+				o.ray = mul(_CamToWorld, o.ray);
+
+				return o;
+			}
+
+			float distanceField(float3 p)
+			{
+				float modX = pMod1(p.x, _modInterval.x);
+				float modY = pMod1(p.y, _modInterval.y);
+				float modZ = pMod1(p.z, _modInterval.z);
+				float Sphere1 = sdSphere(p - _sphere1.xyz, _sphere1.w);
+				float Box1 = sdBox(p - _box1.xyz, _box1.www);
+				return opS(Sphere1, Box1);
+			}
+
+			float3 getNormal(float3 p)
+			{
+				const float2 offset = float2(0.001, 0.0);
+				float3 n = float3(
+					distanceField(p + offset.xyy) - distanceField(p - offset.xyy),
+					distanceField(p + offset.yxy) - distanceField(p - offset.yxy),
+					distanceField(p + offset.yyx) - distanceField(p - offset.yyx));
+				return normalize(n);			
+			}
+
+			fixed4 raymarching(float3 ro, float3 rd, float depth)
+			{
+				fixed4 result = fixed4(1,1,1,1);
+				const int max_interation = 1920;
+				float t = 0; //distance travelled along the ray direction
+
+				for ( int i=0; i< max_interation; i++)
+				{
+					if ( t > _maxDistance || t > depth)
+					{
+						//Evironment
+						result = fixed4(rd,0);
+						break;
+					}
+
+					float3 p = ro + rd * t;
+					//check for hit in distancefield
+					float d = distanceField(p);
+					if (d < 0.01) //We have hit something
+					{
+						//shading!
+						float3 n= getNormal(p);
+						float light = dot(-_LightDir, n);
+						result = fixed4(_mainColor.rgb * light,1);
+						break;
+					}
+					t += d;
+				}
+
+				return result;
+			}
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float depth = LinearEyeDepth(tex2D(_CameraDepthTexture, i.uv).r);
+				depth *= length(i.ray);
+				fixed3 col = tex2D(_MainTex, i.uv);
+				float3 rayDirection = normalize(i.ray.xyz);
+				float3 rayOrigin = _WorldSpaceCameraPos;
+				fixed4 result = raymarching(rayOrigin, rayDirection, depth);
+				return fixed4(col * (1.0 - result.w) + result.xyz * result.w ,1.0);
+			}
+			ENDCG
+		}
+	}
+}
